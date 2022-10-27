1e4eb3e592d808fc63456e4ce834c04d3fc7c1ff
juanmendez7 <juan_esteban.mendez@uao.edu.co>
2022-10-27 18:56:48 -0500

    ROTACION DE PLATAFORMA CON PERSONAJE
    Rotan las plataformas y el personaje se mueve con ellas

---
+++ Assets/Scripts/AgarrePlataforma.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgarrePlataforma : MonoBehaviour
+{
+
+    public GameObject Player;
+    
+    // Start is called before the first frame update
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject == Player)
+        {
+            Player.transform.parent = transform;
+            
+        }
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject == Player)
+        {
+            Player.transform.parent = null;
+            
+        }
+    }
+}
---
+++ Assets/Scripts/RotacionSobreMismo.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotacionSobreMismo : MonoBehaviour
+{
+
+    public GameObject ObjetoEstatico;
+    public float velocidadRotar;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(ObjetoEstatico.transform.position, Vector3.up, velocidadRotar * Time.deltaTime);
+    }
+}
