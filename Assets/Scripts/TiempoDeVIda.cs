6e0ae03127f99c0c91d4bd582c213ea089c4e315
Miguelleon2 <110541766+Miguelleon2@users.noreply.github.com>
2022-11-08 10:40:26 -0500

    Menumelo
    Menu y otras vainas

---
+++ Assets/Scripts/TiempoDeVIda.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TiempoDeVIda : MonoBehaviour
+{
+    public float tiempodevida;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject , tiempodevida);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
