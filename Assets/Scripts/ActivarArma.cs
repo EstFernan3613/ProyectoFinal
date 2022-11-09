0901eaa433eefa073665f1ca290057862cf802aa
juanmendez7 <juan_esteban.mendez@uao.edu.co>
2022-11-09 16:32:23 -0500

    Escena 2 y espada para personaje
    escena 2 y espada de personaje

---
+++ Assets/Scripts/ActivarArma.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivarArma : MonoBehaviour
+{
+
+    public CogerArma cogerArmas;
+    public int numeroArma;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cogerArmas = GameObject.FindGameObjectWithTag("Player").GetComponent<CogerArma>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            cogerArmas.ActivarArmas(numeroArma);
+            Destroy(gameObject);
+        }
+    }
+}
