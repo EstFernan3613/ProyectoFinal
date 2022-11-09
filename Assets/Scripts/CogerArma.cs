88b5174db566991f308afe61eac6b1b524463e64
juanmendez7 <juan_esteban.mendez@uao.edu.co>
2022-11-09 18:02:54 -0500

    Enemigo muerte
    se muere el enemigo xd

--- Assets/Scripts/CogerArma.cs
+++ Assets/Scripts/CogerArma.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class CogerArma : MonoBehaviour
 {
+    public BoxCollider[] armasBoxCol;
+    public BoxCollider puñoBoxCol;
+
 
     public GameObject[] armas;
     public LogPersonajeP logicaPersonaje1;
     // Start is called before the first frame update
     void Start()
     {
-        
+        DesactivarCollidersArmas();
     }
 
     // Update is called once per frame
@@ -41,6 +44,37 @@ public class CogerArma : MonoBehaviour
 
             logicaPersonaje1.conArma = false;
         }  
+    }
+
+    public void ActivarCollidersArmas()
+    {
+        for(int i = 0; i < armasBoxCol.Length; i++)
+        {
+            if(logicaPersonaje1.conArma)
+            {
+                if(armasBoxCol[i] != null)
+                {
+                    armasBoxCol[i].enabled = true;
+                }
+            }
+            else
+            {
+                puñoBoxCol.enabled = true;
+            }
+        }
+    }
+
+
+    public void DesactivarCollidersArmas()
+    {
+        for(int i = 0; i < armasBoxCol.Length; i++)
+        {
+            if(armasBoxCol[i] != null)
+            {
+                armasBoxCol[i].enabled = false;
+            }
+        }
+        puñoBoxCol.enabled = false;
     }
 
 
